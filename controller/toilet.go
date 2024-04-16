2c16d0a51868175d7ff97e68b1cf17bbe58644b6
airy-swift <thanks.xa@gmail.com>
2024-04-17 08:33:34 +0900

    データ修正

--- controller/toilet.go
+++ controller/toilet.go
@@ -11,14 +11,14 @@ func ToiletList(c *gin.Context) {
 	//BookLists := bookService.GetBookList()
 
 	var p = []*model.Toilet{
-		{0, "hello", 0},
-		{0, "hello", 0},
-		{0, "hello", 0},
-		{0, "hello", 0},
-		{0, "hello", 0},
-		{0, "hello", 0},
-		{0, "hello", 0},
-		{0, "hello", 0},
+		{0, "apple", 0},
+		{1, "banana", 0},
+		{2, "camel", 0},
+		{3, "doctor", 0},
+		{4, "ero", 0},
+		{5, "fuck", 0},
+		{6, "g-spot", 0},
+		{7, "H", 0},
 	}
 
 	c.JSONP(http.StatusOK, gin.H{
